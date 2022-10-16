6a978400c67ae2f14bc4dcd6f2f78084b7df3f1a
ChanYoungHan <sanbon0812@naver.com>
2022-10-16 23:31:06 +0900

    first problems

--- category/greedy/pro2864.py
+++ category/greedy/pro2864.py
@@ -1,3 +1,12 @@
 def solution(input):
+    import re
 
-    return None
+    in1, in2 = input.split(" ")
+    # 5 -> 5, 6 -> 5 when get min value
+    patern_chage_6 = re.compile("6")
+    min = int(patern_chage_6.sub("5", in1)) + int(patern_chage_6.sub("5", in2))
+
+    # 5 -> 6, 6 -> 6 when get max value
+    patern_chage_5 = re.compile("5")
+    max = int(patern_chage_5.sub("6", in1)) + int(patern_chage_5.sub("6", in2))
+    return str(min) + " " + str(max)
