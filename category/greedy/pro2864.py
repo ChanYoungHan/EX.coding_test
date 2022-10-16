189540155d18a9c06f27a3d0f3892d9dd45eda12
ChanYoungHan <sanbon0812@naver.com>
2022-10-16 23:10:35 +0900

    judgement of success

--- category/greedy/pro2864.py
+++ category/greedy/pro2864.py
@@ -1,2 +1,3 @@
 def solution(input):
-    return "output"
+
+    return None
--- interface.py
+++ interface.py
@@ -2,17 +2,20 @@ from pathlib import Path
 import re
 
 from input_output.inout_interface import get_inout_string
+from category.greedy.pro2864 import solution
 
 input_list = [x for x in Path("input_output").glob("input*")]
 print(input_list)
 
-from category.solution_interface import solution
-
 patern = re.compile("input*")
 for idx, input_file in enumerate(input_list):
-    print(f"examle {idx + 1} of total {len(input_list)} \n I/O :  ")
     io = get_inout_string(
         input_file, Path("input_output", patern.sub("output", input_file.name))
     )
-    print(io)
-    print(solution(io.input))
+    print(f"examle {idx + 1} of total {len(input_list)} \n I/O : {io}")
+    output = solution(io.input)
+    if io.output == output:
+        mark = "Success"
+    else:
+        mark = "Fail"
+    print(f" [{mark}] output : {output}")
